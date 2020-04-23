1b4cc150a5029886067ab862e396e10560cb0a7d
Andrey Schmider <shmider272244@mail.ru>
2020-04-23 20:05:52 +0600

    commit

---
+++ window/window/Form1.Designer.cs
@@ -0,0 +1,190 @@
+﻿namespace window
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.treeView1 = new System.Windows.Forms.TreeView();
+            this.imageList1 = new System.Windows.Forms.ImageList(this.components);
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.imageList2 = new System.Windows.Forms.ImageList(this.components);
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.treeView1);
+            this.panel1.Location = new System.Drawing.Point(3, 41);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(208, 305);
+            this.panel1.TabIndex = 0;
+            // 
+            // treeView1
+            // 
+            this.treeView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.treeView1.ImageIndex = 0;
+            this.treeView1.ImageList = this.imageList1;
+            this.treeView1.Location = new System.Drawing.Point(0, 0);
+            this.treeView1.Name = "treeView1";
+            this.treeView1.SelectedImageIndex = 0;
+            this.treeView1.Size = new System.Drawing.Size(208, 305);
+            this.treeView1.TabIndex = 0;
+            this.treeView1.AfterSelect += new System.Windows.Forms.TreeViewEventHandler(this.treeView1_AfterSelect);
+            // 
+            // imageList1
+            // 
+            this.imageList1.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("imageList1.ImageStream")));
+            this.imageList1.TransparentColor = System.Drawing.Color.Transparent;
+            this.imageList1.Images.SetKeyName(0, "directory_mini.bmp");
+            this.imageList1.Images.SetKeyName(1, "file_mini.bmp");
+            this.imageList1.Images.SetKeyName(2, "localdriver.png");
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Назад";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(94, 11);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Вперед";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.listView1);
+            this.panel2.Location = new System.Drawing.Point(218, 41);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(287, 305);
+            this.panel2.TabIndex = 3;
+            // 
+            // listView1
+            // 
+            this.listView1.HideSelection = false;
+            this.listView1.LargeImageList = this.imageList2;
+            this.listView1.Location = new System.Drawing.Point(-3, 0);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(287, 305);
+            this.listView1.TabIndex = 0;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.SelectedIndexChanged += new System.EventHandler(this.listView1_SelectedIndexChanged);
+            this.listView1.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.listView1_MouseDoubleClick);
+            // 
+            // imageList2
+            // 
+            this.imageList2.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("imageList2.ImageStream")));
+            this.imageList2.TransparentColor = System.Drawing.Color.Transparent;
+            this.imageList2.Images.SetKeyName(0, "directory.bmp");
+            this.imageList2.Images.SetKeyName(1, "file.bmp");
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(218, 11);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(206, 20);
+            this.textBox1.TabIndex = 4;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(175, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(39, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Путь:";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(430, 9);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(67, 23);
+            this.button3.TabIndex = 6;
+            this.button3.Text = "Найти";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            this.button3.KeyDown += new System.Windows.Forms.KeyEventHandler(this.button3_KeyDown);
+            // 
+            // Form1
+            // 
+            this.AcceptButton = this.button3;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(509, 349);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.panel1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "winSearch";
+            this.panel1.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TreeView treeView1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.ImageList imageList1;
+        private System.Windows.Forms.ImageList imageList2;
+    }
+}
+
---
+++ window/window/Form1.cs
@@ -0,0 +1,356 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+using System.Collections;
+
+namespace window
+{
+    public partial class Form1 : Form
+    {
+
+        //коллекция посещённых адрессов
+        ArrayList Adresses = new ArrayList();
+        //индекс текущего адресса из коллекции Adresses
+        int currIndex = -1;
+        //текущий адресс
+        string currListViewAdress = "";
+
+        public Form1()
+        {
+
+            this.AcceptButton = this.button3;
+            InitializeComponent();
+            
+            //заполнение TreeView узлами локальных дисков и заполнение дочерних узлов этих дисков
+            string[] str = Environment.GetLogicalDrives();
+            int n = 1;
+            foreach (string s in str)
+            {
+                try
+                {
+                    TreeNode tn = new TreeNode();
+                    tn.Name = s;
+                    tn.Text = "Локальный диск " + s;
+                    treeView1.Nodes.Add(tn.Name, tn.Text, 2);
+                    FileInfo f = new FileInfo(s);
+                    string t = "";
+                    string[] str2 = Directory.GetDirectories(s);
+                    foreach (string s2 in str2)
+                    {
+                        t = s2.Substring(s2.LastIndexOf('\\') + 1);
+                        ((TreeNode)treeView1.Nodes[n - 1]).Nodes.Add(s2, t, 0);
+                    }
+                }
+                catch { }
+                n++;
+            }
+            foreach (TreeNode tn in treeView1.Nodes)
+            {
+                for (int i = 65; i < 91; i++)
+                {
+                    char sym = Convert.ToChar(i);
+                    if (tn.Name == sym + ":\\")
+                        tn.SelectedImageIndex = 2;
+                }
+            }
+
+        }
+       
+        private void treeView1_AfterSelect(object sender, TreeViewEventArgs e)
+        {
+            string strtmp = "";
+            if (Adresses.Count != 0)
+            {
+                strtmp = ((string)Adresses[Adresses.Count - 1]);
+                Adresses.Clear();
+                Adresses.Add(strtmp);
+                currIndex = 0;
+            }
+            Adresses.Add(e.Node.Name);
+            currIndex++;
+            //проверка возможности перехода назад/вперёд
+            if (currIndex + 1 == Adresses.Count)
+                button2.Enabled = false;
+            else
+                button2.Enabled = true;
+            if (currIndex - 1 == -1)
+                button1.Enabled = false;
+            else
+                button1.Enabled = true;
+            listView1.Items.Clear();
+            currListViewAdress = e.Node.Name;
+            textBox1.Text = currListViewAdress;
+            //заполнение ListView
+            try
+            {
+                
+                    FileInfo f = new FileInfo(e.Node.Name);
+                    string t = "";
+                    string[] str2 = Directory.GetDirectories(e.Node.Name);
+                    ListViewItem lw = new ListViewItem();
+                    foreach (string s2 in str2)
+                    {
+                        f = new FileInfo(s2);
+                        t = s2.Substring(s2.LastIndexOf('\\') + 1);
+                        lw = new ListViewItem(new string[] { t, "", f.LastWriteTime.ToString() }, 0);
+                        lw.Name = s2;
+                        listView1.Items.Add(lw);
+                    }
+                    str2 = Directory.GetFiles(e.Node.Name);
+                    foreach (string s2 in str2)
+                    {
+                        f = new FileInfo(s2);
+                        t = s2.Substring(s2.LastIndexOf('\\') + 1);
+                        lw = new ListViewItem(new string[] { t, f.Length.ToString(), f.LastWriteTime.ToString() }, 1);
+                        lw.Name = s2;
+                        listView1.Items.Add(lw);
+                    }
+                
+                
+            }
+            catch { }
+
+            int i = 0;
+            //заполнение дочерних узлов дочерними узлами развёртываемого узала
+            try
+            {
+                foreach (TreeNode tn in e.Node.Nodes)
+                {
+                    string[] str2 = Directory.GetDirectories(tn.Name);
+                    foreach (string str in str2)
+                    {
+                        TreeNode temp = new TreeNode();
+                        temp.Name = str;
+                        temp.Text = str.Substring(str.LastIndexOf('\\') + 1);
+                        e.Node.Nodes[i].Nodes.Add(temp);
+                    }
+                    i++;
+                }
+            }
+            catch { }
+
+        }
+      
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //обработка "Назад"
+            if (currIndex - 1 != -1)
+            {
+                currIndex--;
+                currListViewAdress = ((string)Adresses[currIndex]);
+                if (currIndex + 1 == Adresses.Count)
+                    button2.Enabled = false;
+                else
+                    button2.Enabled = true;
+                if (currIndex - 1 == -1)
+                    button2.Enabled = false;
+                else
+                    button1.Enabled = true;
+                textBox1.Text = currListViewAdress;
+                FileInfo f = new FileInfo(currListViewAdress);
+                string t = "";
+                string[] str2 = Directory.GetDirectories(currListViewAdress);
+                string[] str3 = Directory.GetFiles(currListViewAdress);
+                listView1.Items.Clear();
+                ListViewItem lw = new ListViewItem();
+
+                foreach (string s2 in str2)
+                {
+                    f = new FileInfo(s2);
+                    t = s2.Substring(s2.LastIndexOf('\\') + 1);
+                    lw = new ListViewItem(new string[] { t, "", f.LastWriteTime.ToString() }, 0);
+                    lw.Name = s2;
+                    listView1.Items.Add(lw);
+                }
+                foreach (string s2 in str3)
+                {
+                    f = new FileInfo(s2);
+                    t = s2.Substring(s2.LastIndexOf('\\') + 1);
+                    lw = new ListViewItem(new string[] { t, f.Length.ToString(), f.LastWriteTime.ToString() }, 1);
+                    lw.Name = s2;
+                    listView1.Items.Add(lw);
+                }
+
+
+
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //обработка "Вперёд"
+            if (currIndex + 1 != Adresses.Count)
+            {
+                currIndex++;
+                currListViewAdress = ((string)Adresses[currIndex]);
+                if (currIndex + 1 == Adresses.Count)
+                    button2.Enabled = false;
+                else
+                    button2.Enabled = true;
+                if (currIndex - 1 == -1)
+                    button1.Enabled = false;
+                else
+                    button1.Enabled = true;
+                textBox1.Text = currListViewAdress;
+                FileInfo f = new FileInfo(currListViewAdress);
+                string t = "";
+                string[] str2 = Directory.GetDirectories(currListViewAdress);
+                string[] str3 = Directory.GetFiles(currListViewAdress);
+                listView1.Items.Clear();
+                ListViewItem lw = new ListViewItem();
+
+                foreach (string s2 in str2)
+                {
+                    f = new FileInfo(s2);
+                    t = s2.Substring(s2.LastIndexOf('\\') + 1);
+                    lw = new ListViewItem(new string[] { t, "", f.LastWriteTime.ToString() }, 0);
+                    lw.Name = s2;
+                    listView1.Items.Add(lw);
+                }
+                foreach (string s2 in str3)
+                {
+                    f = new FileInfo(s2);
+                    t = s2.Substring(s2.LastIndexOf('\\') + 1);
+                    lw = new ListViewItem(new string[] { t, f.Length.ToString(), f.LastWriteTime.ToString() }, 1);
+                    lw.Name = s2;
+                    listView1.Items.Add(lw);
+                }
+
+
+            }
+
+
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+            //переход в указанную директорию по нажатию на кнопку
+
+            try
+            {
+              
+                string[] str2 = Directory.GetDirectories(textBox1.Text);
+                string[] str3 = Directory.GetFiles(textBox1.Text);
+                currIndex++;
+                currListViewAdress = textBox1.Text;
+                Adresses.Add(textBox1.Text);
+                if (currIndex + 1 == Adresses.Count)
+                    button2.Enabled = false;
+                else
+                    button2.Enabled = true;
+                if (currIndex - 1 == -1)
+                    button1.Enabled = false;
+                else
+                    button1.Enabled = true;
+                FileInfo f = new FileInfo(textBox1.Text);
+                string t = "";
+                listView1.Items.Clear();
+                ListViewItem lw = new ListViewItem();
+
+                foreach (string s2 in str2)
+                {
+                    f = new FileInfo(s2);
+
+                    t = s2.Substring(s2.LastIndexOf('\\') + 1);
+                    lw = new ListViewItem(new string[] { t, "", f.LastWriteTime.ToString() }, 0);
+                    lw.Name = s2;
+                    listView1.Items.Add(lw);
+                }
+                foreach (string s2 in str3)
+                {
+                    f = new FileInfo(s2);
+
+                    t = s2.Substring(s2.LastIndexOf('\\') + 1);
+                    lw = new ListViewItem(new string[] { t, f.Length.ToString(), f.LastWriteTime.ToString() }, 1);
+                    lw.Name = s2;
+                    listView1.Items.Add(lw);
+                }
+            }
+
+
+
+            catch
+            {
+                textBox1.Text = currListViewAdress;
+            }
+           
+        }
+
+        private void button3_KeyDown(object sender, KeyEventArgs e)
+        {
+
+        }
+
+        private void listView1_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            if (listView1.SelectedItems[0].Text.IndexOf('.') == -1)
+            {
+                //обработка нажатия на папку
+                Adresses.Add(listView1.SelectedItems[0].Name);
+                currIndex++;
+                currListViewAdress = ((string)Adresses[currIndex]);
+                
+                if (currIndex + 1 == Adresses.Count)
+                    button2.Enabled = false;
+                else
+                    button2.Enabled = true;
+                if (currIndex - 1 == -1)
+                    button1.Enabled = false;
+                else
+                    button1.Enabled = true;
+                currListViewAdress = listView1.SelectedItems[0].Name;
+                textBox1.Text = currListViewAdress;
+                FileInfo f = new FileInfo(listView1.SelectedItems[0].Name);
+                string t = "";
+                string[] str2 = Directory.GetDirectories(listView1.SelectedItems[0].Name);
+                string[] str3 = Directory.GetFiles(listView1.SelectedItems[0].Name);
+                listView1.Items.Clear();
+                ListViewItem lw = new ListViewItem();
+
+                foreach (string s2 in str2)
+                {
+                    f = new FileInfo(s2);
+                    t = s2.Substring(s2.LastIndexOf('\\') + 1);
+                    lw = new ListViewItem(new string[] { t, "", f.LastWriteTime.ToString() }, 0);
+                    lw.Name = s2;
+                    listView1.Items.Add(lw);
+                }
+                foreach (string s2 in str3)
+                {
+                    f = new FileInfo(s2);
+                    
+                    t = s2.Substring(s2.LastIndexOf('\\') + 1);
+                    lw = new ListViewItem(new string[] { t, f.Length.ToString(), f.LastWriteTime.ToString() }, 1);
+                    lw.Name = s2;
+                    listView1.Items.Add(lw);
+                }
+            }
+            else
+            {
+                //обработка нажатия на файл(его запуска)
+                System.Diagnostics.Process MyProc = new System.Diagnostics.Process();
+                MyProc.StartInfo.FileName = listView1.SelectedItems[0].Name;
+                MyProc.Start();
+            }
+        }
+
+        private void listView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
